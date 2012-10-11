d72c43851b24735a89625661ea767d6abfb01fcd
Andrey Usov <ownport@gmail.com>
2012-10-11 19:59:07 +0300

    added downloadinf robots.txt

--- pywsinfo.py
+++ pywsinfo.py
@@ -5,7 +5,6 @@
 
 # TODO collect WHOIS informatio
 # TODO extract meta information from homepage (head, meta)
-# TODO add robots.txt support
 # TODO add sitemap.xml support
 # TODO add choice to select different report types about website
 # TODO add choice to select different report format (text, json, html) about website
@@ -18,32 +17,69 @@ import pprint
 import requests
 import urlparse
 
+# default setting for python-requests
+REQUESTS_DEFAULTS = {}
+
 def parse_url(url):  
     ''' parse website url, remove path if exists '''
     url_parts = urlparse.urlparse(url)
     return {
-            'website.url': urlparse.urlunsplit((url_parts.scheme,url_parts.netloc,'','','')),
-            'website.host': url_parts.netloc
+            'source_url': urlparse.urlunsplit((url_parts.scheme,url_parts.netloc,'','','')),
+            'host': url_parts.netloc
     }
 
 def nslookup(host):
     ''' returns result of DNS lookup '''
     # return socket.gethostbyname(host)
-    return socket.gethostbyname_ex(host)[2]
+    try:
+        return socket.gethostbyname_ex(host)[2]
+    except:
+        return []
 
 class WebsiteInfo(object):
     ''' website information '''    
     
     def __init__(self, website_url):    
         self._url = website_url
-        self._details = self._parse_url(self._url)
+        self._homepage_content = None
+        self._details = parse_url(self._url)
     
     def _make_request(self, method, url):
-        pass
+        ''' make request to website '''        
+        result = dict()
+        resp = requests.request(method, url)
+        
+        result['final_url'] = resp.url
+        result['status_code'] = resp.status_code
+        if 'server' in resp.headers:
+            result['server'] = resp.headers['server']
+        if 'powered-by' in resp.headers:
+            result['powered-by'] = resp.headers['x-powered-by']
+
+        if resp.content:
+            result['content'] = resp.content
+
+        return result
     
     def run(self):
         ''' run website info retrieval '''
-        self._details['website.ip_addreses'] = self._nslookup(self._details['website.host'])
+        
+        # nslookup
+        self._details['ip_addreses'] = nslookup(self._details['host'])
+
+        # first request to web site. Try to detect 
+        if len(self._details['ip_addreses']) > 0:
+            resp = self._make_request('GET', self._details['source_url'])
+            for k in resp.keys():
+                self._details[k] = resp[k]
+
+        # check robots.txt
+        if self._details.get('status_code') == 200:
+            robots_url = urlparse.urljoin(self._details['final_url'],'/robots.txt')
+            resp = self._make_request('GET', robots_url)
+            if 'content' in resp:
+                self._details['robots.txt'] = resp['content']
+                
     
     def report(self):
         ''' website report '''
