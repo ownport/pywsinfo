759927ebfe2caef0846a06461ab0c20b50bb6cd4
Andrey Usov <ownport@gmail.com>
2012-10-12 07:30:39 +0300

    added handling sitemaps in robots.txt and feafult sitemap.xml

--- pywsinfo.py
+++ pywsinfo.py
@@ -8,14 +8,17 @@
 # TODO add sitemap.xml support
 # TODO add choice to select different report types about website
 # TODO add choice to select different report format (text, json, html) about website
+# TODO read robots.txt from string
 
 __author__  = 'Andrey Usov <https://github.com/ownport/pywsinfo>'
 __version__ = '0.1'
 
+import re
 import socket
 import pprint
 import requests
 import urlparse
+import datetime
 
 # default setting for python-requests
 REQUESTS_DEFAULTS = {}
@@ -60,6 +63,24 @@ class WebsiteInfo(object):
             result['content'] = resp.content
 
         return result
+
+    def _handle_html_head(self, content):
+        ''' handle HTML head, extract keywords & description '''
+        
+        result = dict()
+        
+        content = content.replace('\r', '')
+        content = content.replace('\n', '')
+        head = ''.join(re.findall(r'<head>(.+?)</head>', content, re.I))
+        metas = re.findall(r'<meta(.+?)[\/]?>', head, re.I)
+        for meta in metas:
+            meta_dict = dict(re.findall(r'(\w+)\s*=\s*"(.+?)"', meta, re.I))
+            if 'name' in meta_dict:
+                if meta_dict['name'].lower() == 'keywords':
+                    result['keywords'] = [c.strip() for c in meta_dict['content'].split(',')]
+                if meta_dict['name'].lower() == 'description':
+                    result['description'] = meta_dict['content']
+        return result
     
     def run(self):
         ''' run website info retrieval '''
@@ -71,18 +92,36 @@ class WebsiteInfo(object):
         if len(self._details['ip_addreses']) > 0:
             resp = self._make_request('GET', self._details['source_url'])
             for k in resp.keys():
-                self._details[k] = resp[k]
+                if k == 'content':
+                    head_params = self._handle_html_head(resp[k])
+                    for k in head_params:
+                        self._details[k] = head_params[k]
+                else:
+                    self._details[k] = resp[k]
 
         # check robots.txt
         if self._details.get('status_code') == 200:
             robots_url = urlparse.urljoin(self._details['final_url'],'/robots.txt')
             resp = self._make_request('GET', robots_url)
-            if 'content' in resp:
+            if resp['status_code'] == 200 and 'content' in resp:
                 self._details['robots.txt'] = resp['content']
+                # TODO extract link to sitemap.xml
+                for line in self._details['robots.txt'].split('\n'):
+                    if line.startswith('Sitemap'):
+                        if 'sitemaps' not in self._details:
+                            self._details['sitemaps'] = list()
+                        self._details['sitemaps'].append(line.replace('Sitemap:', '').strip())
 
-        # check sitemap
-        if 'robots.txt' in self._details:
-            print self._details['robots.txt']                
+        # check default sitemap
+        if 'sitemaps' not in self._details:
+            sitemaps_url = urlparse.urljoin(self._details['final_url'],'/sitemap.xml')
+            resp = self._make_request('HEAD', sitemaps_url) 
+            if resp['status_code'] == 200:
+                self._details['sitemaps'] = sitemaps_url
+            
+    
+        # latest update datetime
+        self._details['last_update'] = str(datetime.datetime.now())
     
     def report(self):
         ''' website report '''
